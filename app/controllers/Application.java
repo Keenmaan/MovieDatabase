1ee6f2304f2d741ccb8a32290fd5abb5a89642d5
Keenmaan <keenmaan@gmail.com>
2015-06-28 14:00:39 +0200

    Added all views and set proper routes.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,6 +7,18 @@ import views.html.*;
 
 public class Application extends Controller {
 
+    public Result topGenreView() {
+        return Results.TODO;
+    }
+
+    public Result searchView() {
+        return Results.TODO;
+    }
+
+    public Result movieDescriptionView(int id) {
+        return Results.TODO;
+    }
+
     public Result index() {
         return ok(index.render());
     }
