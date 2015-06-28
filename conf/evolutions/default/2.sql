0f099b6bdd8c0a5529bf8c5a62bb7eab59fbf09d
Keenmaan <keenmaan@gmail.com>
2015-06-28 13:20:48 +0200

    Inserted movie data to the db via evolutions.

---
+++ conf/evolutions/default/2.sql
@@ -0,0 +1,24 @@
+
+# --- !Ups
+INSERT INTO genre VALUES (28, 'Action');
+INSERT INTO genre VALUES (12, 'Adventure');
+INSERT INTO genre VALUES (16, 'Animation');
+INSERT INTO genre VALUES (35, 'Comedy');
+INSERT INTO genre VALUES (80, 'Crime');
+INSERT INTO genre VALUES (99, 'Documentary');
+INSERT INTO genre VALUES (18, 'Drama');
+INSERT INTO genre VALUES (10751, 'Family');
+INSERT INTO genre VALUES (14, 'Fantasy');
+INSERT INTO genre VALUES (10769, 'Foreign');
+INSERT INTO genre VALUES (36, 'History');
+INSERT INTO genre VALUES (27, 'Horror');
+INSERT INTO genre VALUES (10402, 'Music');
+INSERT INTO genre VALUES (9648, 'Mystery');
+INSERT INTO genre VALUES (10749, 'Romance');
+INSERT INTO genre VALUES (878, 'Science Fiction');
+INSERT INTO genre VALUES (10770, 'TV Movie');
+INSERT INTO genre VALUES (53, 'Thriller');
+INSERT INTO genre VALUES (10752, 'War');
+INSERT INTO genre VALUES (37, 'Western');
+
+# --- !Downs
